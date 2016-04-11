84f6579dfce3b3747a0cd687b382619d80bbedb7
Dakeryas <mrdakeryas@gmail.com>
2016-04-11 17:47:26 +0200

    Removed TimeWindow dependency on CosmogenicHunter::Bounds<double> and added operator>>

--- include/TimeWindow.hpp
+++ include/TimeWindow.hpp
@@ -1,15 +1,18 @@
 #ifndef COSMOGENIC_ANALYSER_TIME_WINDOW_H
 #define COSMOGENIC_ANALYSER_TIME_WINDOW_H
 
-#include "Cosmogenic/Bounds.hpp"
+#include <iomanip>
+#include <stdexcept>
+#include <regex>
 
 namespace CosmogenicAnalyser{
 
   class TimeWindow{
     
-    CosmogenicHunter::Bounds<double> timeBounds;
+    double startTime, endTime;
     
   public:
+    TimeWindow() = default;
     TimeWindow(double startTime, double endTime);
     TimeWindow(const TimeWindow& other) = default;
     TimeWindow(TimeWindow&& other) = default;
@@ -19,12 +22,14 @@ namespace CosmogenicAnalyser{
     double getStartTime() const;
     double getEndTime() const;
     double getLength() const;
+    void setStartEndTimes(double startTime, double endTime);
     bool covers(double time) const;//check if the time is within the time window (accept events in [startTime, endTime[ )
     virtual void print(std::ostream& output) const;
     
   };
 
   std::ostream& operator<<(std::ostream& output, const TimeWindow& window);
+  std::istream& operator>>(std::istream& input, TimeWindow& window);
 
 }
 
--- src/TimeWindow.cpp
+++ src/TimeWindow.cpp
@@ -2,37 +2,62 @@
 
 namespace CosmogenicAnalyser{
 
-  TimeWindow::TimeWindow(double startTime, double endTime):timeBounds(startTime, endTime){
+  TimeWindow::TimeWindow(double startTime, double endTime):startTime(startTime),endTime(endTime){
+    
+    if(startTime < 0 || endTime < 0 || endTime < startTime){
+      
+      auto errorMessage = std::to_string(startTime)+" and "+std::to_string(endTime)+" are not valid start and end times.";
+      throw std::invalid_argument(errorMessage);
+      
+    }
     
   }
 
   double TimeWindow::getStartTime() const{
     
-    return timeBounds.getLowEdge();
+    return startTime;
 
   }
   
   double TimeWindow::getEndTime() const{
     
-    return timeBounds.getUpEdge();
+    return endTime;
 
   }
 
   double TimeWindow::getLength() const{
     
-    return timeBounds.getWidth();
+    return endTime - startTime;
+
+  }
+  
+  void TimeWindow::setStartEndTimes(double startTime, double endTime){
+    
+    if(startTime < 0 || endTime < 0 || endTime < startTime){
+      
+      auto errorMessage = std::to_string(startTime)+" and "+std::to_string(endTime)+" are not valid start and end times.";
+      throw std::invalid_argument(errorMessage);
+      
+    }
+    else{
+      
+      this->startTime = startTime;
+      this->endTime = endTime;
+      
+    }
 
   }
 
   bool TimeWindow::covers(double time) const{
 
-    return timeBounds.contains(time);
+    if(time >= startTime && time < endTime) return true;
+    else return false;
 
   }
   
   void TimeWindow::print(std::ostream& output) const{
     
-    output<<"["<<std::setw(5)<<std::internal<<timeBounds.getLowEdge()<<", "<<std::setw(5)<<std::internal<<timeBounds.getUpEdge()<<"]";
+    output<<"["<<std::setw(5)<<std::internal<<startTime<<", "<<std::setw(5)<<std::internal<<endTime<<"]";
     
   }
 
@@ -41,6 +66,21 @@ namespace CosmogenicAnalyser{
     window.print(output);
     return output;
     
+  }
+  
+  std::istream& operator>>(std::istream& input, TimeWindow& window){
+  
+    std::string token;
+    input >> token;
+    
+    std::string number("[+-]?(?:\\d*\\.)?\\d+(?:[eE][-+]?[0-9]+)?");//decimal number with possible sign and exponent
+    std::regex regex("(^"+number+")[:,]("+number+"$)");//start with a number :, seprator and end with another number
+    std::smatch regexMatches;
+    if(std::regex_search(token, regexMatches, regex)) window.setStartEndTimes(std::stod(regexMatches[1]), std::stod(regexMatches[2]));
+    else throw std::invalid_argument(token+" cannot be parsed to build the time window.");
+    
+    return input;
+  
   }
 
 }
