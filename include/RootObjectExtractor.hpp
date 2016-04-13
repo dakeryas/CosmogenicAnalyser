2d00dab131ebb19a4f59f8f8e715aa63bcbccf8f
Dakeryas <mrdakeryas@gmail.com>
2016-04-13 12:45:08 +0200

    Added RootObjectExtractor<T> empty class with T* specialisation to extract TKey's from ROOT files

---
+++ include/RootObjectExtractor.hpp
@@ -0,0 +1,45 @@
+#ifndef COSMOGENIC_ANALYSER_ROOT_OBJECT_EXTRACTOR_H
+#define COSMOGENIC_ANALYSER_ROOT_OBJECT_EXTRACTOR_H
+
+#include "TFile.h"
+#include <iostream>
+
+namespace CosmogenicAnalyser{
+  
+  template <class T>
+  class RootObjectExtractor{
+    
+  public:
+    static T extract(const std::string& keyName, TFile& file);
+    
+  };
+  
+  template <class T>
+  T RootObjectExtractor<T>::extract(const std::string& keyName, TFile& file){
+    
+    auto object = dynamic_cast<T*>(file.Get(keyName.c_str()));
+    if(object != nullptr) return *object;
+    else throw std::runtime_error("Could not extact '"+keyName+"' from "+file.GetName());
+    
+  }
+  
+  template <class T>
+  class RootObjectExtractor<T*>{
+    
+  public:
+    static T* extract(const std::string& keyName, TFile& file);
+    
+  };
+  
+  template <class T>
+  T* RootObjectExtractor<T*>::extract(const std::string& keyName, TFile& file){
+    
+    auto object = dynamic_cast<T*>(file.Get(keyName.c_str()));
+    if(object != nullptr) return object;
+    else throw std::runtime_error("Could not extact '"+keyName+"' from "+file.GetName());
+    
+  }
+  
+}
+
+#endif
